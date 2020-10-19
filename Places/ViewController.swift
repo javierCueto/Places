ff45ff1a7999059b2617721b9ad36a8a1e195d09
javierCueto <javelie@outlook.com>
2020-10-19 10:14:24 -0600

    Initial Commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by José Javier Cueto Mejía on 19/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
